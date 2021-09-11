edc0856f0c4edf54fdb0f8d0d6b7cdaedc606921
Avaer Kazmer <a@modules.io>
2021-09-10 20:47:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import * as THREE from 'three';
 import metaversefile from 'metaversefile';
 const {useApp, useFrame} = metaversefile;
 
+const size = 0.5;
+const scale = 0.3;
+const q90 = new THREE.Quaternion()
+  .setFromUnitVectors(
+    new THREE.Vector3(0, 0, 1),
+    new THREE.Vector3(0, 1, 0)
+  );
+  
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
 const localVector3 = new THREE.Vector3();
@@ -9,30 +17,16 @@ const localVector4 = new THREE.Vector3();
 const localQuaternion = new THREE.Quaternion();
 const localMatrix = new THREE.Matrix4();
 
-const q90 = new THREE.Quaternion()
-  .setFromUnitVectors(
-    new THREE.Vector3(0, 0, 1),
-    new THREE.Vector3(0, 1, 0)
+const arrowGeometry = new THREE.PlaneBufferGeometry(size, size)
+  .applyMatrix4(
+    new THREE.Matrix4()
+      .makeRotationFromQuaternion(
+        new THREE.Quaternion()
+          .setFromAxisAngle(new THREE.Vector3(0, 0, 1), Math.PI)
+      )
   );
-
-export default () => {
-  const app = useApp();
-  
-  const size = 0.5;
-  const scale = 0.3;
-
-  const geometry = new THREE.PlaneBufferGeometry(size, size)
-	  .applyMatrix4(
-		  new THREE.Matrix4()
-			  .makeRotationFromQuaternion(
-				  new THREE.Quaternion()
-					  .setFromAxisAngle(new THREE.Vector3(0, 0, 1), Math.PI)
-			  )
-	  );
-
-  // console.log('got bounding box', boundingBox);
-
-	const u = `${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}_Down Tap Note 16x16.png`;
+const arrowMaterial = (() => {
+  const u = `${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}_Down Tap Note 16x16.png`;
   (async () => {
     const img = await new Promise((accept, reject) => {
       const img = new Image();
@@ -46,18 +40,18 @@ export default () => {
     tex.image = img;
     tex.needsUpdate = true;
   })();
-	const tex = new THREE.Texture();
-	// tex.minFilter = THREE.NearestFilter;
-	tex.magFilter = THREE.NearestFilter;
+  const tex = new THREE.Texture();
+  // tex.minFilter = THREE.NearestFilter;
+  tex.magFilter = THREE.NearestFilter;
 
-	const material = new THREE.ShaderMaterial({
+  const material = new THREE.ShaderMaterial({
     uniforms: {
       tex: {
         type: 't',
         value: tex,
         needsUpdate: true,
       },
-			uTime: {
+      uTime: {
         type: 'f',
         value: 0,
         needsUpdate: true,
@@ -70,8 +64,8 @@ export default () => {
       void main() {
         vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
         gl_Position = projectionMatrix * mvPosition;
-				
-				vUv = uv;
+        
+        vUv = uv;
       }
     `,
     fragmentShader: `\
@@ -79,18 +73,18 @@ export default () => {
       precision highp int;
       #define PI 3.1415926535897932384626433832795
       uniform sampler2D tex;
-			uniform float uTime;
+      uniform float uTime;
       varying vec2 vUv;
       void main() {
-				float t = floor(uTime * 16. * 16.);
-				float x = mod(t, 16.);
-				// float y = floor((uTime - x) / 16.);
-				float y = 0.;
-				vec2 uv = (vUv / 16.0) + vec2(x, y)/16.;
+        float t = floor(uTime * 16. * 16.);
+        float x = mod(t, 16.);
+        // float y = floor((uTime - x) / 16.);
+        float y = 0.;
+        vec2 uv = (vUv / 16.0) + vec2(x, y)/16.;
         gl_FragColor = texture2D(tex, uv);
-				if (gl_FragColor.a < 0.9) {
-				  discard;
-				}
+        if (gl_FragColor.a < 0.9) {
+          discard;
+        }
       }
     `,
     transparent: true,
@@ -99,7 +93,81 @@ export default () => {
     // polygonOffsetFactor: -1,
     // polygonOffsetUnits: 1,
   });
-  const mesh = new THREE.Mesh(geometry, material);
+  return material;
+})();
+const tailMaterial = (() => {
+  const u = `${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}tail.png`;
+  (async () => {
+    const img = await new Promise((accept, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        // document.body.appendChild(img);
+        accept(img);
+      };
+      img.onerror = reject;
+      img.crossOrigin = 'Anonymous';
+      img.src = u;
+    });
+    tex.image = img;
+    tex.needsUpdate = true;
+  })();
+  const tex = new THREE.Texture();
+  // tex.minFilter = THREE.NearestFilter;
+  // tex.magFilter = THREE.NearestFilter;
+  tex.wrapS = THREE.RepeatWrapping;
+  tex.wrapT = THREE.RepeatWrapping;
+
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      tex: {
+        type: 't',
+        value: tex,
+        needsUpdate: true,
+      },
+      uTime: {
+        type: 'f',
+        value: 0,
+        needsUpdate: true,
+      },
+    },
+    vertexShader: `\
+      precision highp float;
+      precision highp int;
+      varying vec2 vUv;
+      void main() {
+        vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+        gl_Position = projectionMatrix * mvPosition;
+        
+        vUv = uv;
+      }
+    `,
+    fragmentShader: `\
+      precision highp float;
+      precision highp int;
+      #define PI 3.1415926535897932384626433832795
+      uniform sampler2D tex;
+      uniform float uTime;
+      varying vec2 vUv;
+      void main() {
+        // gl_FragColor = vec4(1., 0., 0., 1.);
+        gl_FragColor = texture2D(tex, vec2(vUv.x, vUv.y + uTime));
+        // gl_FragColor.rgb *= 1. + vUv.y;
+        // gl_FragColor.a = pow(vUv.y, 0.5);
+      }
+    `,
+    transparent: true,
+    side: THREE.DoubleSide,
+    polygonOffset: true,
+    polygonOffsetFactor: 1,
+    polygonOffsetUnits: 1,
+  });
+  return material;
+})();
+
+export default () => {
+  const app = useApp();
+
+  const mesh = new THREE.Mesh(arrowGeometry, arrowMaterial);
   mesh.scale.setScalar(scale);
   mesh.frustumCulled = false;
   // mesh.visible = false;
@@ -122,73 +190,7 @@ export default () => {
     let positionIndex = 0;
     let indexIndex = 0;
     let maxIndexIndex = 0;
-    
-    const u = `${import.meta.url.replace(/(\/)[^\/]*$/, '$1')}tail.png`;
-    (async () => {
-      const img = await new Promise((accept, reject) => {
-        const img = new Image();
-        img.onload = () => {
-          // document.body.appendChild(img);
-          accept(img);
-        };
-        img.onerror = reject;
-        img.crossOrigin = 'Anonymous';
-        img.src = u;
-      });
-      tex.image = img;
-      tex.needsUpdate = true;
-    })();
-    const tex = new THREE.Texture();
-    // tex.minFilter = THREE.NearestFilter;
-    // tex.magFilter = THREE.NearestFilter;
-    tex.wrapS = THREE.RepeatWrapping;
-    tex.wrapT = THREE.RepeatWrapping;
-    
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        tex: {
-          type: 't',
-          value: tex,
-          needsUpdate: true,
-        },
-        uTime: {
-          type: 'f',
-          value: 0,
-          needsUpdate: true,
-        },
-      },
-      vertexShader: `\
-        precision highp float;
-        precision highp int;
-        varying vec2 vUv;
-        void main() {
-          vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-          gl_Position = projectionMatrix * mvPosition;
-          
-          vUv = uv;
-        }
-      `,
-      fragmentShader: `\
-        precision highp float;
-        precision highp int;
-        #define PI 3.1415926535897932384626433832795
-        uniform sampler2D tex;
-        uniform float uTime;
-        varying vec2 vUv;
-        void main() {
-          // gl_FragColor = vec4(1., 0., 0., 1.);
-          gl_FragColor = texture2D(tex, vec2(vUv.x, vUv.y + uTime));
-          // gl_FragColor.rgb *= 1. + vUv.y;
-          // gl_FragColor.a = pow(vUv.y, 0.5);
-        }
-      `,
-      transparent: true,
-      side: THREE.DoubleSide,
-      polygonOffset: true,
-      polygonOffsetFactor: 1,
-      polygonOffsetUnits: 1,
-    });
-    const tailMesh = new THREE.Mesh(geometry, material);
+    const tailMesh = new THREE.Mesh(geometry, tailMaterial);
     // tailMesh.position.y = -height/2;
     // tailMesh.drawMode = THREE.TriangleStripDrawMode;
     tailMesh.frustumCulled = false;
